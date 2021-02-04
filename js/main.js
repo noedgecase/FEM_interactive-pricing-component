84fd6acaa7472e1387cd6f81f46a65f353d4ec34
popescudragos <dragos042@yahoo.com>
2021-02-04 02:25:31 +0200

    progress ui + js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict'
 
+const checkBox = document.querySelector('.checkbox');
+const switchBtn = document.querySelector('.switch-container');
+const rangeSlider = document.getElementById('range-slider');
+
+window.addEventListener('load', ()=>{
+      checkBox.checked=false;
+      console.log('unchecked');
+});
+
+checkBox.addEventListener('click', ()=>{
+            switchBtn.classList.toggle('active');
+})
